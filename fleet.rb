1ad0eafe126a27b4259be01df0faceade94504f7
Simon Griffault <Simon.griffault@ucdconnect.ie>
2017-09-25 19:07:28 +0100

    initialization of classes

--- fleet.rb
+++ fleet.rb
@@ -1,3 +1,33 @@
+class Fleet
+  def initialize(numb)
+    @number = numb
+  end
+end
 
-class Flette
+class Car
+  def initialize(id, litres, km)
+    @id = id
+    @litres_in_tank = litres
+    @km_per_litre = km
+  end
+end
+
+class Trip(id, distance)
+  def initialize
+    @id = id
+    @distance = distance
+  end
+
+end
+
+class TripCollection(nb)
+  def initialize
+    @numbers_of_trips = nb
+  end
+end
+
+class TripRoster
+  def initialize()
+    @collection_pairs = []
+  end
 end
